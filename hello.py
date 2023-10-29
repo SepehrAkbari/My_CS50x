c3a2ae21603e5485f74bdbf592f308a4a30196d7
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2023-10-28 22:41:10 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello world")
+speed_of_light = 300000000
+
+def calculate_energy(mass):
+    energy = mass * speed_of_light ** 2
+    return energy
+
+def main():
+    mass = int(input("m: "))
+
+    energy = calculate_energy(mass)
+
+    print(f"E: {energy}")
+
+if __name__ == "__main__":
+    main()
