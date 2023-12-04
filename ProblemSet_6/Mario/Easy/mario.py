1ed0109772a27687d13a8d54fe5266c0d7ada7ff
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2023-12-03 22:25:51 -0600

    Create mario.py

---
+++ ProblemSet_6/Mario/Easy/mario.py
@@ -0,0 +1,13 @@
+# TODO
+h = 0
+while True:
+    try:
+        h = int(input("Height: "))
+        if 1 <= h <= 8:
+            break
+    except ValueError:
+        print("Please enter a valid integer.")
+
+for i in range(1, h + 1):
+    print(" " * (h - i) + "#" * i)
+  
